8bead1d1f8649b32ca133bc9a4e8b242e48c2dee
nutszebra <nutszebra@gmail.com>
2019-05-02 22:44:32 +0900

    a

--- models.py
+++ models.py
@@ -131,8 +131,8 @@ class YOLOLayer(nn.Module):
         FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
         self.stride = self.img_dim / self.grid_size
         # Calculate offsets for each grid
-        self.grid_x = torch.arange(g).repeat(g, 1).view([1, 1, g, g]).type(FloatTensor)
-        self.grid_y = torch.arange(g).repeat(g, 1).t().view([1, 1, g, g]).type(FloatTensor)
+        self.register_buffer('grid_x', torch.arange(g).repeat(g, 1).view([1, 1, g, g]).type(FloatTensor))
+        self.register_buffer('grid_y', torch.arange(g).repeat(g, 1).t().view([1, 1, g, g]).type(FloatTensor))
         self.scaled_anchors = FloatTensor([(a_w / self.stride, a_h / self.stride) for a_w, a_h in self.anchors])
         self.anchor_w = self.scaled_anchors[:, 0:1].view((1, self.num_anchors, 1, 1))
         self.anchor_h = self.scaled_anchors[:, 1:2].view((1, self.num_anchors, 1, 1))
